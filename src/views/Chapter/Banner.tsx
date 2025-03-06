c97d28ddf2c3a7c872a0d01f7babf62ce5e86147
galuhssaputra <galuhputrana576@gmail.com>
2025-03-06 13:27:02 +0700

    two commits

--- src/views/Chapter/Banner.tsx
+++ src/views/Chapter/Banner.tsx
@@ -3,6 +3,7 @@ import Button from "@/components/Button/Button";
 import Switcher from "@/components/QuranReader/Switcher";
 import { ChapterInfo, Chapters } from "@/types";
 import { PropType, defineComponent } from "vue";
+import { useSettings } from "@/hooks/settings";
 import "@/components/Layout/bism.css";
 
 export default defineComponent({
@@ -16,11 +17,13 @@ export default defineComponent({
             required: true
         }
     },
+    setup() {
+        const settings = useSettings();
+        return { settings };
+    },
     render() {
         return (
             <>
-                
-
                 <div class="row d-flex justify-content-between mb-5 mt-5">
                     <div class="col-6 col-md-4">
                         <Switcher />
@@ -29,27 +32,26 @@ export default defineComponent({
                         <PlayAudioButton audioId={this.chapter.id} />
                     </div>
                 </div>
+                
+
+                
                 {this.chapter.bismillah_pre && (
                     <div class="container-fluid d-flex justify-content-center mb-3">
                         <img class="img-fluid bismillah-img" src="/assets/svg/bismillah.svg" />
                     </div>
                 )}
-
-                <div class="d-flex align-items-center justify-content-center text-white p-3 shadow-md mb-5">
-                    <h4 class="mb-0">
+                <div class="d-flex align-items-center justify-content-center p-3 shadow-md mb-5">
+                    <h4 class={`mb-0 ${this.settings.isDarkMode.value ? 'text-white' : 'text-dark'}`}>
                         {this.chapter.name_simple}
                     </h4>
                     <router-link 
                         to={{ name: "chapter.info", params: { id: this.$route.params.id } }} 
-                        class="text-white ms-2"
+                        class="ms-2"
                     >
                         <font-awesome-icon icon="info-circle" size="2x" />
                     </router-link>
                 </div>
-                
-                
             </>
-
         )
     }
 })
