142e57575e08e5e9e51b12e53e0b25ac64ca1184
galuhssaputra <galuhputrana576@gmail.com>
2025-03-24 08:42:12 +0700

    two commit

--- src/views/Home/Juz/Page.tsx
+++ src/views/Home/Juz/Page.tsx
@@ -24,7 +24,7 @@ export default defineComponent({
     const juzs = ref<Data>([]);
 
     juzs_response.value = (
-      await httpRetry.get<{ juzs: Juzs[] }>(makeUrl("juzs"),)
+      await httpRetry.get<{ juzs: Juzs[] }>(makeUrl("juzs"))
     ).data.juzs.map((item) => ({
       ...item,
       chapters: Object.keys(item.verse_mapping)
@@ -51,29 +51,36 @@ export default defineComponent({
     };
   },
   render() {
-    // console.log(this.data);
     return (
-      <div class="d-flex flex-wrap gap-2">  
-          {this.data.map(chapter => (
+      <div>
+        <div class="d-flex flex-wrap gap-2">
+          {[...Array(30)].map((_, index) => {
+            const juzNumber = index + 1; // Nomor juz dimulai dari 1 hingga 30
+            return (
               <div
-                  key={chapter.id}
-                  class={[styles.card_chapter, styles.border_radius_1rem]}
-                  style={{ width: "82px",}} 
-                  onClick={() => this.$router.push({ name: "juz", params: { id: chapter.id } })}
+                key={juzNumber}
+                class={[styles.card_chapter, styles.border_radius_1rem]}
+                style={{ width: "82px" }}
+                onClick={() => this.$router.push({ name: "juz", params: { id: juzNumber } })}
               >
-                  <div class="d-flex justify-content-between h-100">
-                      <div class="d-flex align-items-center">
-                          <div class="me-1 position-relative d-flex align-items-center text-center">
-                              <Icon class={styles.svg_icon} name="stars-islamic" width={60} height={60} />
-                              <span class="fw-bold h6 position-absolute text-primary" style="transform: translate(-50%, -50%);left: 50%;top: 50%">
-                                  {chapter.id}
-                              </span>
-                          </div>
-                      </div>
-                  </div>  
+                <div class="d-flex justify-content-between h-100">
+                  <div class="d-flex align-items-center">
+                    <div class="me-1 position-relative d-flex align-items-center text-center">
+                      <Icon class={styles.svg_icon} name="stars-islamic" width={60} height={60} />
+                      <span
+                        class="fw-bold h6 position-absolute text-primary"
+                        style="transform: translate(-50%, -50%); left: 50%; top: 50%"
+                      >
+                        {juzNumber}
+                      </span>
+                    </div>
+                  </div>
+                </div>
               </div>
-          ))}
+            );
+          })}
+        </div>
       </div>
-    )
-  }
+    );
+  },
 });
